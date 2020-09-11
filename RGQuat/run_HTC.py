38d42d34a76bdf9c7a4cce43697289dc44d34105
Michael Gerard <mjgerard2@wisc.edu>
2020-09-11 18:28:06 -0500

    print HTC output

--- RGQuat/run_HTC.py
+++ RGQuat/run_HTC.py
@@ -24,12 +24,9 @@ def run_sim(grain):
             
 def main(num_of_procs, num_of_grains):
     ### Gernerate Dipole ###
-    #var.dipole_position = np.array([0,0,var.r_m - np.random.choice(var.depth_pop)])
-    #var.dipole_moment = np.array([0,0,np.random.choice(var.moment_pop)])
+    var.dipole_position = np.array([0,0,var.r_m - np.random.choice(var.depth_pop)])
+    var.dipole_moment = np.array([0,0,np.random.choice(var.moment_pop)])
     
-    var.dipole_position = np.array([0,0,var.r_m-1e5])
-    var.dipole_moment = np.array([0,0,1e10])
-    '''
     theta = 2*np.pi*np.random.random()
     phi = np.pi*np.random.random()
     
@@ -42,7 +39,7 @@ def main(num_of_procs, num_of_grains):
                        [sin_theta, cos_theta*cos_phi, -cos_theta*sin_phi],
                        [0, sin_phi, cos_phi]])
     var.dipole_moment = np.matmul(matrix, var.dipole_moment)
-    '''
+    
     ### Instantiate Grains ###
     grains = np.array([tDG.dust_grain() for i in range(num_of_grains)])
     
@@ -64,18 +61,15 @@ def main(num_of_procs, num_of_grains):
               'lift fail' : 0,
               'never fail' : 0}
     
-    finCon = np.empty((num_of_grains, 7))
     for idx, res in enumerate(results):
-        finCon[idx] = res[0]
         for key in res[1]:
             counts[key] = counts[key] + res[1][key]
-    
-    ### Save Meta Data ###
-    an.create_meta(counts, os.path.join(os.getcwd(), 'meta_data.txt'), time_data)
-    with hf.File(os.path.join(os.getcwd(),'data.h5'), 'w') as hf_file:
-        hf_file.create_dataset('data', data=finCon)
+        print('{0} {1} {2} {3} {4} {5} {6}'.format(res[0][0,0],res[0][0,1],res[0][0,2],res[0][0,3],res[0][0,4],res[0][0,5],res[0][0,6]))
 
+    an.create_meta(counts, os.path.join(os.getcwd(), 'meta_data.txt'), time_data)
+    
+    
 if __name__=='__main__':
-    num_of_procs = int(sys.argv[1])
-    num_of_grains = int(sys.argv[2])
+    num_of_procs = 2#int(sys.argv[1])
+    num_of_grains = 10#int(sys.argv[2])
     main(num_of_procs, num_of_grains)
